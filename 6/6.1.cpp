2fb8ea412a00db2c26cf6e18993b8ad81da4c59f
Pluciarz <124045496+Pluciarz@users.noreply.github.com>
2025-02-05 20:19:41 +0100

    Add files via upload

---
+++ 6/6.1.cpp
@@ -0,0 +1,35 @@
+﻿// 6.1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int l, n=10;
+    while (n!=0)
+    {
+        l = rand() % 6 + 1;
+        cout << "Liczba wylosowana na kostce to: " << l << endl;
+        if (l == 6)
+        {
+            cout << "Wylosowano 6 za " << 10 - n + 1 << " razem";
+            break;
+        }
+        n--;
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
