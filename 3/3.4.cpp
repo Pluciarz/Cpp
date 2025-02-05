663a73f8b463bdee7c378db0a15a6b50428e84ba
Pluciarz <124045496+Pluciarz@users.noreply.github.com>
2025-02-05 20:10:00 +0100

    Add files via upload

---
+++ 3/3.4.cpp
@@ -0,0 +1,42 @@
+﻿// 3.4.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+float Oblicz_BMI(float waga, float wzrost)
+{
+    return waga / (pow(wzrost, 2));
+}
+int main()
+{
+    float waga, wzrost;
+    cout << "Podaj wage: ";
+    cin >> waga;
+    cout << "Podaj wzrost: ";
+    cin >> wzrost;
+    cout << "Podales ze wazysz " << waga << " kg, a wzrost twoj wynosi " << wzrost << " m" << endl;
+    float BMI = Oblicz_BMI(waga, wzrost);
+    if (BMI < 18.5)
+    {
+        cout << "Masz niedowage";
+    }
+    else if (BMI >= 25)
+    {
+        cout << "Masz nadwage";
+    }
+    else
+    {
+        cout << "Twoja waga jest prawidlowa";
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ 3/3.6.cpp
@@ -0,0 +1,60 @@
+﻿// 3.6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float a, b, c;
+
+float delta(float a, float b, float c)
+{
+   float delta;
+   return delta = (b * b) - (4 * a * c);
+}
+
+int main()
+{
+    
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    cout << "Podaj c: ";
+    cin >> c;
+    if (a == 0)
+    {
+        cout << "Nie mozna obliczyc delty";
+    }
+    else
+    {
+        if (delta(a,b,c) < 0)
+        {
+            cout << "Brak miejsc zerowych";
+        }
+        else if (delta(a,b,c) == 0)
+        {
+            cout << "1 miejsce zerowe" << endl;
+            float p = -b / (2 * a);
+            cout << "Miejsce zerowe wynosi " << p;
+        }
+        else
+        {
+            cout << "2 miejsce zerowe" << endl;
+            float x1 = (-b - sqrt(delta(a,b,c))) / (2 * a);
+            float x2 = (-b + sqrt(delta(a,b,c))) / (2 * a);
+            cout << "Miejsca zerowe wynosza " << x1 << " i " << x2;
+        }
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
